93d9ddae26418545c3513accbd9091efbcb53114
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-03 16:36:55 -0300

    Agregado proyecto base de spring boot y hibernate

---
+++ src/test/java/com/frro/bus/ticket/BusTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frro.bus.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
