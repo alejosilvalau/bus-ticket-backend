91a8b6ae6cbd9059cc2089ad0fb23f9a185268e9
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-05 23:17:05 -0300

    Adaptado schema para respetar herencia en hibernate

--- docs/schema.sql
+++ docs/schema.sql
@@ -2,11 +2,6 @@ create database if not exists bus_ticket;
 
 use bus_ticket;
 
-create table person_role (
-  id int auto_increment primary key,
-  role varchar(100) not null unique
-);
-
 create table person (
   id int auto_increment primary key,
   first_name varchar(100) not null,
@@ -15,10 +10,9 @@ create table person (
   email varchar(100) null unique,
   password varchar(128) null,
   is_admin boolean null default false,
+  id_user boolean null default true,
   license_number varchar(50) null unique,
-  phone_number varchar(20) null,
-  id_role int not null,
-  constraint fk_person_role foreign key (id_role) references person_role (id) on delete restrict on update cascade
+  phone_number varchar(20) null
 );
 
 create table location (
