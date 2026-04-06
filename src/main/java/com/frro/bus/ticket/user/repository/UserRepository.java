f7c0d31af52888f8f9d912c107a775b0e60619fb
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-05 21:31:27 -0300

    Eliminados repository, controller y service de person

--- src/main/java/com/frro/bus/ticket/user/repository/UserRepository.java
+++ src/main/java/com/frro/bus/ticket/user/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.frro.bus.ticket.user.repository;
 
-import com.frro.bus.ticket.person.repository.PersonRepository;
 import com.frro.bus.ticket.user.model.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface UserRepository extends PersonRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
 
