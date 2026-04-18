990f78e3fa576c97072e0b6952a338aed3c936a5
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-18 07:33:26 -0300

    Terminado los controladores de usuario

--- src/main/java/com/frro/bus/ticket/features/identity/controllers/UserProfileController.java
+++ src/main/java/com/frro/bus/ticket/features/identity/controllers/UserProfileController.java
@@ -1,20 +1,43 @@
-// Delete, Update
-//
+package com.frro.bus.ticket.features.identity.controllers;
 
-// @PutMapping("/{id}")
-// public ResponseEntity<UserResponse> update(@PathVariable int id, @RequestBody
-// UserRequest userRequest) {
-// Optional<UserResponse> updatedUser = userService.update(id, userRequest);
-// return updatedUser.map(ResponseEntity::ok)
-// .orElseGet(() -> ResponseEntity.notFound().build());
-// }
-//
-//
-// @DeleteMapping("/{id}")
-// public ResponseEntity<UserResponse> delete(@PathVariable int id) {
-// Optional<UserResponse> deletedUser = userService.delete(id);
-// return deletedUser.map(ResponseEntity::ok)
-// .orElseGet(() -> ResponseEntity.notFound().build());
-// }
-//
-// Logical delete as well
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.frro.bus.ticket.features.identity.dtos.user.UpdateUserDTO;
+import com.frro.bus.ticket.features.identity.dtos.user.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/profile")
+@RequiredArgsConstructor
+public class UserProfileController {
+    @PatchMapping("/update")
+    public ResponseEntity<UserDTO> update(@RequestBody UpdateUserDTO userRequest) {
+        Optional<UserDTO> updatedUser = userService.update(userRequest);
+        return updatedUser.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<UserDTO> delete() {
+        Optional<UserDTO> deletedUser = userService.delete();
+        return deletedUser.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/logical-delete")
+    public ResponseEntity<UserDTO> logicalDelete() {
+        Optional<UserDTO> updatedUser = userService.logicalDelete();
+        return updatedUser.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
