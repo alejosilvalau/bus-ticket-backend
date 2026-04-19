9bca2c6821811b996f01dd3875c573a6233a2afc
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-18 22:53:21 -0300

    Corregidas las relaciones de entidad ticket

--- src/main/java/com/frro/bus/ticket/features/booking/entities/Ticket.java
+++ src/main/java/com/frro/bus/ticket/features/booking/entities/Ticket.java
@@ -2,8 +2,10 @@ package com.frro.bus.ticket.features.booking.entities;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
+import com.frro.bus.ticket.features.fleet.entities.Seat;
+import com.frro.bus.ticket.features.identity.entities.Person;
 import com.frro.bus.ticket.features.journey.entities.Trip;
 
 @Entity
@@ -13,9 +15,21 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(nullable = false)
+    private BigDecimal finalPrice;
+
+    @Column(nullable = false)
+    private ZonedDateTime bookingTime;
+
+    @Column(nullable = false)
+    private boolean isCancelled = false;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_user", nullable = false)
-    private com.frro.bus.ticket.features.identity.entities.Person user;
+    private Person user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_trip", nullable = false)
@@ -23,17 +37,5 @@ public class Ticket {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_seat", nullable = false)
-    private com.frro.bus.ticket.features.fleet.entities.Seat seat;
-
-    @Column(nullable = false)
-    private BigDecimal finalPrice;
-
-    @Column(nullable = false)
-    private LocalDateTime bookingTime;
-
-    @Column(nullable = false)
-    private boolean isCancelled;
-
-    @Column(nullable = false, unique = true)
-    private String token;
+    private Seat seat;
 }
