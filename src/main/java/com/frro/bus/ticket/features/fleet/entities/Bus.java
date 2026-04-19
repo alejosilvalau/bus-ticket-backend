4dc3400e815a3fe8b152d6af77c16f3c9d085952
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-19 16:30:23 -0300

    Actualizada la relacion de Bus para respetar la relacion de dependencia de bus y seat

--- src/main/java/com/frro/bus/ticket/features/fleet/entities/Bus.java
+++ src/main/java/com/frro/bus/ticket/features/fleet/entities/Bus.java
@@ -32,6 +32,6 @@ public class Bus {
     @OneToMany(mappedBy = "bus", fetch = FetchType.LAZY)
     private List<Trip> trips;
 
-    @OneToMany(mappedBy = "bus", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "bus", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<Seat> seats;
 }
